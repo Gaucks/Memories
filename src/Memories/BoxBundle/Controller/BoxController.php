3589b4c45b524080d19299f2b89e02efd3b34554
Gaucks <Gaucks@gmail.com>
2014-03-23 17:12:33 +0100

    Update V0.1.1

--- src/Memories/BoxBundle/Controller/BoxController.php
+++ src/Memories/BoxBundle/Controller/BoxController.php
@@ -7,12 +7,24 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class BoxController extends Controller
 {
     public function indexAction()
+    {						
+        return $this->render('MemoriesBoxBundle:Box:index.html.twig' );
+    }
+    
+    public function addFolderAction()
+    {	
+	    return $this->render('MemoriesBoxBundle:Box:Add/addFolder.html.twig');
+    }
+    
+    public function MenuAction()
     {
     	$folder = array(
-							array('folder' => 'Symfony2', 'memotitle' => array('titre' => array('rigolo', 'lol' ) )  ,  'date' => new \Datetime() ),
-							array('folder' => 'Jquery'  , 'memotitle' => array('titre' => array('La programmation c\'est cool', 'un teste', 'un autre')), 'date' => new \Datetime())
-    					);
-    						
-        return $this->render('MemoriesBoxBundle:Box:index.html.twig', array('folder' => $folder) );
+						array('folder' => 'Symfony2', 'memotitle' => array('titre' => 
+																	 array('rigolo', 'lol' ) ), 'date' => new \Datetime() ),
+						array('folder' => 'Jquery'  , 'memotitle' => array('titre' => 
+																	 array('La programmation c\'est cool', 
+																	 	   'un teste', 'un autre')),'date' => new \Datetime()));
+																	 	   
+	    return $this->render('MemoriesBoxBundle:Box:userMenu.html.twig', array('folder' => $folder ));
     }
 }
